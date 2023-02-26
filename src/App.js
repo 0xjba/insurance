fe4d9019a33e11b471099ae591ffc41b0fc44881
Jobin Babu Ayathil <jobinb6444@outlook.com>
2023-02-27 00:18:22 +0530

    Updated Contract Address to latest deployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import myContractAbi from './Insurance.json'
 function App() {
   return (
     <div className="App">
-      <Insure contractAddress="0x0020407a683d8AA20A22F2230cFa0D8057F3FDf4" contractAbi={myContractAbi} />
+      <Insure contractAddress="0xcb1E1927016fb407CB95B092B111a7E5C0c4b1C0" contractAbi={myContractAbi} />
     </div>
   );
 }
